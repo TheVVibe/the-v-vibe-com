fc69143a8fc569403384c1589129455694e02fdc
Rishikesh Darandale <Rishikesh.Darandale@gmail.com>
2025-12-20 15:53:53 +0530

    feat: add github actions to deploy the website

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  turbopack: {
+    root: ".",
+  },
 };
 
 export default nextConfig;
